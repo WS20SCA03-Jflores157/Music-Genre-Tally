e1aec08ff003d99b4b278dbe9713d16841856ace
Jeffrey <Jeffrey@simpleyapp.com>
2020-04-10 10:03:20 -0400

    Initial Commit

---
+++ Music Genre Tally/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Genre Tally
+//
+//  Created by Jeffrey  on 4/10/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
